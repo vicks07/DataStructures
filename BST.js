70eabd67906c8ca78bfd96a1d09b2c3562f5896c
vicks07 <vikram.january@gmail.com>
2019-09-09 17:01:49 +0530

    Least common ancestor-edit

--- BST.js
+++ BST.js
@@ -7,62 +7,59 @@ class Node{
 }
 
 class BST{
-	constructor(){
-		this.root =null;
-	}
-	insert(data) 
-{
-    var newNode = new Node(data);
-    if(this.root === null) 
-        this.root = newNode; 
-    else
-        this.insertNode(this.root, newNode); 
-} 
-  
-insertNode(node, newNode) 
-{ 
-   if(newNode.data < node.data) 
-    { 
-        if(node.left === null) 
-            node.left = newNode; 
-        else 
-            this.insertNode(node.left, newNode);  
-    } 
-    else
-    { 
-        if(node.right === null) 
-            node.right = newNode; 
-        else
-            this.insertNode(node.right,newNode); 
-    } 
-} 
-inorder(node) 
-{ 
-    if(node !== null) 
-    { 
-        this.inorder(node.left); 
-        console.log(node.data); 
-        this.inorder(node.right); 
-    } 
-} 
-
-getRootNode() 
-{ 
-    return this.root; 
-} 
-	print(){
-	console.log(this.root);
+    constructor(){
+        this.root =null;
     }
+    insert(data) {
+        var newNode = new Node(data);
+        if(this.root === null) 
+            this.root = newNode; 
+        else
+            this.insertNode(this.root, newNode); 
+    } 
     
-    lca(root,val1,val2){
+    insertNode(node, newNode) { 
+    if(newNode.data < node.data) 
+        { 
+            if(node.left === null) 
+                node.left = newNode; 
+            else 
+                this.insertNode(node.left, newNode);  
+        } 
+        else
+        { 
+            if(node.right === null) 
+                node.right = newNode; 
+            else
+                this.insertNode(node.right,newNode); 
+        } 
+    } 
+    inorder(node) { 
+        if(node !== null) 
+        { 
+            this.inorder(node.left); 
+            console.log(node.data); 
+            this.inorder(node.right); 
+        } 
+    } 
+
+    getRootNode() { 
+        return this.root; 
+    } 
+
+    print(){
+        console.log(this.root);
+    }
+        
+    lca(root,val1,val2){ //Least Common ancestor
         //console.log('Root',root.data,val1,val2);
 
-        if(root.data>val1 && root.data>val2){
+        if(root.data > val1 && root.data > val2){
             ////console.log('Left',root.val,val1,val2);
             return this.lca(root.left,val1,val2);
         }
-        else if(root.data<val1 && root.data<val2){
-           // console.log('Right',root.val,val1,val2);
+        else if(root.data < val1 && root.data < val2){
+        // console.log('Right',root.val,val1,val2);
             return this.lca(root.right,val1,val2);
         }
         else{
